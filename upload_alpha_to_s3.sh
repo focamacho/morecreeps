c7ac1160ee886fc7a9b4527682a97c1b6787a3c2
focamacho <renancolombomail@gmail.com>
2022-12-26 17:07:14 -0300

    docs: remove some old files from forge

--- upload_alpha_to_s3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export BUILD_DATE=$(date +%Y%m%d%H%M)
-
-aws s3 cp build/libs/*.jar s3://${BUCKET_NAME}/alpha/morecreeps-alpha-${BUILD_DATE}.jar
-
-aws lambda invoke --function-name morecreeps-discord-post-alpha-build --payload "{\"buildDate\": \"${BUILD_DATE}\"}" /dev/stdout
--- upload_release_to_s3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-aws s3 cp build/libs/*.jar s3://${BUCKET_NAME}/
